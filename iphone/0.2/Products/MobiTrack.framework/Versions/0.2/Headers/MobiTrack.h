2b00806cb53e60158394f100a5b2cf806819d842
Vincent Coste <coste.vincent@gmail.com>
2012-12-30 13:39:51 +0100

    first version

---
+++ iphone/0.2/Products/MobiTrack.framework/Versions/0.2/Headers/MobiTrack.h
@@ -0,0 +1,14 @@
+//
+//  MobiTrack.h
+//  MobiTrackFWKTest
+//
+//  Created by Vincent Coste on 12/24/12.
+//
+//
+
+#ifndef MobiTrackFWKTest_MobiTrack_h
+#define MobiTrackFWKTest_MobiTrack_h
+
+#import "Mobug.h"
+
+#endif
